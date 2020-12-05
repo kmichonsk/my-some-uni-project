b600b45283f198ecc18277afd0f3b6fdf3f728d9
FrosRX <73423134+FrosRX@users.noreply.github.com>
2020-12-05 16:18:17 +0100

    script.js 1.00

---
+++ script.js
@@ -0,0 +1,43 @@
+document.getElementById("main").style.height = window.innerHeight +"px";
+
+function show(x){
+	if(x=="add"){
+        if(document.getElementById("add_form").style.display=="block"){
+            document.getElementById("add_form").style.display = "none";
+            document.getElementById("add_arrow").style.msTransform = "rotate(0deg)"; 
+  		    document.getElementById("add_arrow").style.transform = "rotate(0deg)"; 
+        }
+        else{
+		    document.getElementById("add_form").style.display = "block";
+		    document.getElementById("search_form").style.display = "none";
+		    document.getElementById("add_arrow").style.msTransform = "rotate(180deg)"; 
+  		    document.getElementById("add_arrow").style.transform = "rotate(180deg)"; 
+		    document.getElementById("search_arrow").style.msTransform = "rotate(0deg)"; 
+            document.getElementById("search_arrow").style.transform = "rotate(0deg)"; 
+        }
+	}
+	else if(x=="search"){
+        if(document.getElementById("search_form").style.display=="block"){
+            document.getElementById("search_form").style.display = "none";
+            document.getElementById("search_arrow").style.msTransform = "rotate(0deg)"; 
+  		    document.getElementById("search_arrow").style.transform = "rotate(0deg)"; 
+        }
+        else{
+            document.getElementById("add_form").style.display = "none";
+            document.getElementById("search_form").style.display = "block";
+            document.getElementById("add_arrow").style.msTransform = "rotate(0deg)"; 
+            document.getElementById("add_arrow").style.transform = "rotate(0deg)"; 
+            document.getElementById("search_arrow").style.msTransform = "rotate(180deg)"; 
+            document.getElementById("search_arrow").style.transform = "rotate(180deg)"; 
+        }
+	}
+	else{}
+}
+
+function dodaj_stacje(){
+
+}
+
+function szukaj_stacji(){
+
+}
