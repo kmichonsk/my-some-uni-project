3e53913b272c73d7f86629489fbb704ab1d2acb3
bajka615 <73136258+bajka615@users.noreply.github.com>
2021-05-06 19:58:50 +0200

    update nazw kolumn
    has_fuel -> price
    brand -> brand_id

--- api/src/main/resources/expected-schema.sql
+++ api/src/main/resources/expected-schema.sql
@@ -9,18 +9,20 @@ CREATE TABLE IF NOT EXISTS stations (
     address varchar(39) DEFAULT NULL,
     city varchar(27) DEFAULT NULL,
     postal_code varchar(6) DEFAULT NULL,
-    has_fuel_95 float DEFAULT NULL,
-    has_fuel_98 float DEFAULT NULL,
-    has_fuel_diesel float DEFAULT NULL,
-    has_fuel_lpg float DEFAULT NULL,
-    brand int DEFAULT NULL,
+    price_fuel95 float DEFAULT NULL,
+    price_fuel98 float DEFAULT NULL,
+    price_fuel_diesel float DEFAULT NULL,
+    price_fuel_lpg float DEFAULT NULL,
+    brand_id int DEFAULT NULL,
     opening_hours varchar(255) DEFAULT NULL,
 	latitude float DEFAULT NULL,
-	longitude float DEFAULT NULL
+	longitude float DEFAULT NULL,
+	sum_of_rating int(11) DEFAULT NULL,
+	rating_count int(11) DEFAULT NULL
 ) DEFAULT CHARSET=utf8;
 
-CREATE TABLE `comments` (
-  `id` int(11) NOT NULL,
-  `comment` varchar(255) NOT NULL,
-  `station_id` int(11) NOT NULL
+CREATE TABLE IF NOT EXISTS comments (
+  id int(11) NOT NULL,
+  comment varchar(255) NOT NULL,
+  station_id int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
